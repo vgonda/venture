5c3d1ba8ce9dad51bd3a14f916068d2134071059
vgonda <v.m.gonda@gmail.com>
2015-09-13 23:12:01 +0000

    Create map based on width/height inputs

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,33 +1,41 @@
 import Ember from 'ember';
 
-var MapRow = Ember.Objext.extend({
-columns: []
+var GameMap = Ember.Object.extend({
+  rows: []
+});
+
+var MapRow = Ember.Object.extend({
+  cells: []
 });
 var MapCell = Ember.Object.extend({
-  text: "_"
+  text: "."
 });
 
 export default Ember.Controller.extend({
+  mapWidth: 4,
+  mapHeight: 3,
+
+  map: Ember.computed('mapWidth', 'mapHeight', function() {
+    var w = this.get('mapWidth');
+    var h = this.get('mapHeight');
+    return GameMap.create({rows: buildMapRows(w, h)});
+  }),
 
   init: function() {
-  this.set('name', 'The Hopeful Forest');
-
-    var rows = [
-      MapRow.create({columns: [
-                    MapCell.create(),
-                    MapCell.create(),
-                    MapCell.create(),
-                    ]}),
-      MapRow.create({columns: [
-                    MapCell.create(),
-                    MapCell.create(),
-                    MapCell.create(),
-                    ]}),MapRow.create({columns: [
-                    MapCell.create(),
-                    MapCell.create(),
-                    MapCell.create(),
-                    ]})
-    ];
-    this.set('rows', rows);
-}
+    this.set('name', 'The Hopeful Forest');
+  }
 });
+
+var buildMapRows = function(width, height) {
+  var rows = [];
+  for(var r = 0; r < height; r++) {
+    var cells = [];
+    for (var c = 0; c < width; c++) {
+      var cell = MapCell.create();
+      cells.push(cell);
+    }
+    var row = MapRow.create({cells: cells});
+    rows.push(row);
+  }
+  return rows;
+};
