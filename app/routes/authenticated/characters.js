dafb3830671dc4160fd4e6573de24020b1075a59
vgonda <v.m.gonda@gmail.com>
2015-11-17 02:36:36 +0000

    Create loading page

--- app/routes/authenticated/characters.js
+++ app/routes/authenticated/characters.js
@@ -2,6 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var characters = this.store.findAll('character');
-    return characters;  }
+    let charPromise = this.store.findAll('character');
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      return Ember.run.later(() => {
+        charPromise.then(chars => {
+          if(chars.get('length') !== 0) {
+            resolve(chars);
+          } else {
+            reject({error: "No chars found!"});
+          }
+        });
+      }, 2000);
+    }).then(null, err => {alert(err.error);});
+  },
 });
