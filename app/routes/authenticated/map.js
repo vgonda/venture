30a4c40e4ed4149bf6667499e00e7326747630ff
vgonda <v.m.gonda@gmail.com>
2015-11-17 00:31:28 +0000

    Diff from wednesday

--- app/routes/authenticated/map.js
+++ app/routes/authenticated/map.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.store.findAll('character').then( chars => {
+      return this.store.find('world-view', { character_id: chars.get('firstObjet.id') }).then(() => {alert('wah');});
+    });
+  }
 });
--- app/routes/authenticated/play.js
+++ app/routes/authenticated/play.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function(params) {
+    return this.store.find('character', params.character);
+  },
+
+  serialize: function(model) {
+    return { character: model.get('id') };
+  }
+
 });
